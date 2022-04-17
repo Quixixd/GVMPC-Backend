512db45ae5e86846693a8c0903f2f97fdff2aa1b
Quixixd <90565445+Quixixd@users.noreply.github.com>
2022-04-17 23:58:03 +0200

    ^lwiudwad awd

---
+++ Crimelife/Module/AntiOnePunch/Ati.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GVMP;
+using Crimelife;
+using GTANetworkAPI;
+namespace Crimelife.Module.AntiOnePunch
+{
+    class Ati
+    {
+        [RemoteEvent("onehitcheat")]
+        public void antiHS(Player p)
+        {
+            DbPlayer dbPlayer = p.GetPlayer();
+            WebhookSender.SendMessage("AntiCheat", "Der Spieler " + dbPlayer.Name + " hat warscheinlich den Onehit cheat benutzt!", Webhooks.aclogs, "Admin");
+            p.Kick("OnehitSchlag");
+        }
+    }
+}
---
+++ Crimelife/Module/CRMNL/Zkey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GVMP;
+using Crimelife;
+using GTANetworkAPI;
+namespace Crimelife.Module.AntiOnePunch
+{
+    class ZKey
+    {
+        [RemoteEvent("crmnlcheater")]
+        public void dujude(Player p)
+        {
+            DbPlayer dbPlayer = p.GetPlayer();
+            WebhookSender.SendMessage("AntiCheat", "Der Spieler " + dbPlayer.Name + " hat warscheinlich den Onehit cheat benutzt!", Webhooks.crmnlwarscheinlich, "Admin");
+        }
+    }
+}
